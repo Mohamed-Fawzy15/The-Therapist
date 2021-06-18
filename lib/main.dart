0acb83a06d2589fe10fba4c93fd668a6ed7a1f64
Zhiend15 <mohamed.elsakaa0152gamil.com>
2021-06-18 22:25:02 +0200

    record file

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'Quiz/Quiz.dart';
 import 'Pages/Recommend.dart';
 import 'Pages/DoctorProfile.dart';
 import 'Pages/Role.dart';
+import 'record/record.dart';
 
 
 
@@ -26,22 +27,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Home(),
+      home: RecordScreen(),
     );
   }
 }
